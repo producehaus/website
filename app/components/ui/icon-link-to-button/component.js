bad5958f275ccdfe7d5d54f83a85622d58905e0f
Aram Zadikian <brancusi@gmail.com>
2017-02-24 21:48:50 -0800

    Updated social link

---
+++ app/components/ui/icon-link-to-button/component.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+const IconLinkToButton = Ember.Component.extend({});
+
+IconLinkToButton.reopenClass({
+  positionalParams: ['icon', 'link']
+});
+
+export default IconLinkToButton;
